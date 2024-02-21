40f4a828867f90d82e8afbda6ff150826ee62ccb
seniorman-dev <coder.jay2001@gmail.com>
2024-02-22 00:14:29 +0100

    new file:   android/app/google-services.json 	new file:   ios/Runner/GoogleService-Info.plist 	new file:   ios/firebase_app_id_file.json 	new file:   lib/firebase_options.dart 	modified:   lib/main.dart 	modified:   lib/utils/colors/app_theme.dart 	new file:   macos/Runner/GoogleService-Info.plist 	new file:   macos/firebase_app_id_file.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:spendly/utils/colors/app_theme.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  //keep beneath this widgetflutterbinding widget
+  //usePathUrlStrategy();
+  
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: AppColor.blackColor,
+      statusBarColor: AppColor.blackColor,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ),
+  );
+  
+  //initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //initialize get_storage
+  await GetStorage.init() ;
+
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,21 +47,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- lib/utils/colors/app_theme.dart
+++ lib/utils/colors/app_theme.dart
@@ -7,5 +7,6 @@ import 'package:flutter/material.dart';
 class AppColor {
 
   static const Color bgColor = Color.fromRGBO(18,111,140, 0);
+  static const Color blackColor = Colors.black;
 
 }
